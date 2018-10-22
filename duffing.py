69d01156d69b6ee3a1dc0634fa84275adffdd68a
tomas <tomaskrajco@gmail.com>
2018-10-22 08:02:51 +0200

    duffing

--- duffing.py
+++ duffing.py
@@ -4,7 +4,10 @@ delta = 0.5
 h = 0.004
 gamma = 0.815
 
-w0 = (2*np.pi*20)
+w0 = (2*np.pi*2)
+
+#while w0 > 2*np.pi:
+#    w0 -= 2*np.pi
 
 g_sig = 0
 
@@ -25,26 +28,22 @@ xv =  [x1, x2]
 
 
 dt = h
-t_max = 5.
+t_max = 15.
 t = 0
-amp = 0.09
+amp = 0.07
 sigma = 0.26
 with open('d.txt', 'w') as f:
     while t < t_max:
         r = np.random.normal(0,sigma,1)
-        g_sig = amp*np.cos(2*np.pi*20*t)
+        g_sig = amp*np.cos(2*np.pi*2*t)
         g_sig += r
         # 4th order Runge-Kutta step
         k1 = [ dt * d for d in dxvdt(t, xv) ]
         xv_step = [ xv[i] + k1[i] / 2 for i in range(2) ]
-        g_sig = amp*np.cos(2*np.pi*20*(t + .5 * dt))
-        g_sig += r
         k2 = [ dt *  d for d in dxvdt(t + 0.5 * dt, xv_step) ]
         xv_step = [ xv[i] + k2[i] / 2 for i in range(2) ]
         k3 = [ dt * d for d in dxvdt(t + 0.5 * dt, xv_step) ]
         xv_step = [ xv[i] + k3[i] for i in range(2) ]
-        g_sig = amp*np.cos(2*np.pi*20*(t + dt))
-        g_sig += r
         k4 = [ dt * d for d in dxvdt(t + dt, xv_step) ]
         for i in range(2):
             xv[i] += (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6.0
